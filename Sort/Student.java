56b7964b0b7f714ec302530277c13968b8c30117
Alperen Berk IŞILDAR <alperenberkisildar@gmail.com>
2021-12-09 01:38:41 +0300

    student class(for objects) added

---
+++ Sort/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+	private int number;
+	private int password;
+
+	public Student(int number,int password) {
+		this.number = number;
+		this.password = password;
+	}
+
+	/**
+	 * @return the number
+	 */
+	public int getNumber() {
+		return number;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public int getPassword() {
+		return password;
+	}	
+}
